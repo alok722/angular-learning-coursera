912e964a9c6c484bbcafae2832cc4a6938cf5f2d
Alok Raj <alokr417@gmail.com>
2019-12-29 19:23:05 +0530

    Promise Part 2, simulating 2 secs delay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
@@ -54,6 +55,7 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule,
     MatCardModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     FlexLayoutModule,
     MatDialogModule,
     MatFormFieldModule, 
