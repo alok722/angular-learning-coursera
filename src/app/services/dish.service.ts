bb04967313ce5b3c4f8b195787955c2a011161f9
Alok Raj <alokr417@gmail.com>
2019-12-29 18:58:28 +0530

    Promise Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -9,14 +9,15 @@ export class DishService {
 
   constructor() { }
   
-  getDishes(): Dish[] {
-    return DISHES;
-  }
-  getDish(id: string): Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];
+  getDishes(): Promise<Dish[]> {
+    return Promise.resolve(DISHES);
   }
 
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getDish(id: any): Promise<Dish> {
+    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+  }
+
+  getFeaturedDish(): Promise<Dish> {
+    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
   }
 }
